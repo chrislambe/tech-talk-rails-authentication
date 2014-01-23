df3633c2ccb65723635ef4514aef4d1de9f1d68d
Chris Lambe <chris@chrislambe.com>
2014-01-23 16:17:53 -0600

    Only parse .env if the file exists.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-YAML.load_file(File.join(Rails.root,'.env')).each {|k,v| ENV[k] = v}
+if File.exists? (path = File.join(Rails.root,'.env'))
+  YAML.load_file(path).each {|k,v| ENV[k] = v}
+end
 
 # Initialize the Rails application.
 Feedback::Application.initialize!
